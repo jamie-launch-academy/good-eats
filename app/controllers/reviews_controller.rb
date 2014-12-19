2d3fa7e2c93cb92cf054bed493e13b275514602e
Jamie Francese <jamienf@gmail.com>
2014-12-19 14:46:50 -0500

    added validation and fixed soem errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
 
   def new
-    @restaurant = Restaurant.find_by(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find_by(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant_id = params[:restaurant_id]
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
 
-  validates :rating, presence: true
+  validates :rating, presence: true,
+            :inclusion => { :in => 0..5,
+            :message => " should be between 0 to 5" }
 
   validates :comment, presence: true
 end
